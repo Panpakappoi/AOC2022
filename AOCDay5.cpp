e2f9d3e94b3e22c2effa8806667c3d15465971c7
Panpakappoi <jchan063@gmail.com>
2023-07-02 14:43:23 -0700

    Successfully completed Day 5
    2D Vector solution using double for loops to mimic stack popping and insertion. 2 Loop solution for the 2nd Part as well where you preserve the order of the crates. Not elegant at all.

--- AOCDay5.cpp
+++ AOCDay5.cpp
@@ -19,26 +19,60 @@ struct Extractor {
     uint32_t amount;
     size_t source;
     size_t destination;
-
-    //
-    friend std::istream& operator>>(std::istream& s, Extractor& o) {
-        std::string word;
-        s >> word >> o.amount >> word >> o.source >> o.destination;
-        --o.source; // zero-base;
-        --o.destination; // zero-base
-        return s;
-    }
 };
 
+//Matrix transpose, you want to take it and flip it.
+// read from bottom up and put in stacks.
+// Calculate the character position.
+// String of each line and calculate the character position of each stack
 
+void CrateMover9000(std::vector<Extractor>& instructions, std::vector<std::vector<char>>& state)
+{
+        for (int i = 0; i < instructions.size(); i++)
+        {
+            int amount = instructions[i].amount;
+            int s = instructions[i].source;
+            int d = instructions[i].destination;
+
+            for (int j = 0; j < amount; j++)
+            {
+                state[d].insert(state[d].begin(), state[s][0]);
+                state[s].erase(state[s].begin() + 0);
+            }
+    }
+}
+
+void CrateMover9001(std::vector<Extractor>& instructions, 
+    std::vector<std::vector<char>>& state)
+{
+    for (int i = 0; i < instructions.size(); i++)
+    {
+        int amount = instructions[i].amount;
+        int s = instructions[i].source;
+        int d = instructions[i].destination;
+        for (int j = amount - 1; j >= 0; j--)
+        {
+            state[d].insert(state[d].begin(), state[s][j]);
+            state[s].erase(state[s].begin() + j);
+        }
+    }
+
+}
 
 
 std::vector<Extractor> parseInstructions(std::ifstream& inFile) {
     std::vector<Extractor> instructions;
-    std::string line;
-    while (inFile >> line) {
-        std::cout << line;
+    std::string word;
+    Extractor target;
+    while (inFile >> word >> target.amount >>
+        word >> target.source >> word >> target.destination) {
+        --target.source; //zero base
+        --target.destination; // zero base
+        std::cout << target.amount << " " << target.source << " " << target.destination << "\n";
+        instructions.emplace_back(target);
     }
+
+    return instructions;
 }
 
 
@@ -83,6 +117,15 @@ void Extract_Relevant_Instruction() {
         }
        std::cout << std::endl;
     }
+    std::vector<Extractor> instruction_set = parseInstructions(aocInput);
+    CrateMover9001(instruction_set, result);
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            std::cout << result[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
     aocInput.close();
 }
 
@@ -91,5 +134,6 @@ void Extract_Relevant_Instruction() {
 int main()
 {
     Extract_Relevant_Instruction();
+    
 }
 
